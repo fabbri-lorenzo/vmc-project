966489727be4f75e11b4cd21aa28f16a54e775d0
fabbri-lorenzo <fabbri-lorenzo@users.noreply.github.com>
2024-12-17 23:48:42 +0000

    deploy: 3ba5001251aa6bfefde6b15ae808d32ef296f536

---
+++ search/all_e.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['statfunctype_0',['StatFuncType',['../group__lexic-types.html#ga425e16aae8ec073f46fa42f58a3e96a5',1,'vmcp']]],
+  ['statistic_1',['Statistic',['../group__lexic-types.html#ga165fa5f78e8d7fbf78900b85431cda5b',1,'vmcp']]],
+  ['statistics_2ecpp_2',['statistics.cpp',['../statistics_8cpp.html',1,'']]],
+  ['statistics_2ehpp_3',['statistics.hpp',['../statistics_8hpp.html',1,'']]],
+  ['statistics_2einl_4',['statistics.inl',['../statistics_8inl.html',1,'']]],
+  ['stddev_5',['StdDev',['../group__helpers.html#ga0b020b33be65e1c0a56df7f54c8edd43',1,'vmcp']]],
+  ['stepdenom_5fgraddesc_6',['stepDenom_gradDesc',['../group__algs-constants.html#gadd767fad6d0175c6e8c99f44165ce8a1',1,'vmcp']]],
+  ['stoppingthreshold_5fgraddesc_7',['stoppingThreshold_gradDesc',['../group__algs-constants.html#ga4249a34489a88d26276c2dc26d0b2d01',1,'vmcp']]],
+  ['structure_20types_8',['Structure types',['../group__struct-types.html',1,'']]]
+];
